85b6fb35e2892e5453d1265fc442dfe9aeff9e1a
Amardeep Ranjan <amardeepranjan911@gmail.com>
2023-10-29 10:47:42 +0530

    fixed prisma model

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -7,7 +7,7 @@ const f = createUploadthing();
 const auth = (req: Request) => ({ id: "fakeId" });
 
 export const ourFileRouter = {
-  pdfUploader: f({ image: { maxFileSize: "4MB" } })
+  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
     .middleware(async ({ req }) => {
       const { getUser } = getKindeServerSession();
       const user = getUser();
@@ -22,7 +22,7 @@ export const ourFileRouter = {
           name: file.name,
           userId: metadata.userId,
           url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-          uploadStatus: "PENDING",
+          uploadStatus: "PROCESSING",
         },
       });
     }),
